c69630130184cbe13c24eeb027add8de7778aa4e
achrafbouzaidi <achrafbouzaidi.pf@gmail.com>
2021-06-19 01:03:34 +0100

    This file contain add class and multiply class

---
+++ library.cs
@@ -0,0 +1,11 @@
+public class AddClass{
+    public int Add(int a, int b){
+        return a+b;
+    }
+}
+
+public class MultiplyClass{
+    public int Multiply(int a, int b){
+        return a*b;
+    }
+}
