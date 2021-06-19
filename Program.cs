9b0245cbcc2613a7218f207e46ce4d95dcbc4d1c
achrafbouzaidi <achrafbouzaidi.pf@gmail.com>
2021-06-19 01:13:45 +0100

    The reste of files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AchrafProjet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddClass obj_1 = new AddClass();
+            System.Console.WriteLine(obj_1.Add(2, 3));
+
+            MultiplyClass obj_2 = new MultiplyClass();
+            System.Console.WriteLine(obj_2.Multiply(2, 3));
+
+        }
+    }
+}
